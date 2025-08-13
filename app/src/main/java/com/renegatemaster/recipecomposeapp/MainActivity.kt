8ab1c8bd2a2667dac58e49e75951ae9a214dd3c7
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-08-13 19:15:13 +0500

    RCA-04: Organize the app structure via Scaffold

--- app/src/main/java/com/renegatemaster/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/renegatemaster/recipecomposeapp/MainActivity.kt
@@ -4,22 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.renegatemaster.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RecipeComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            MaterialTheme {
+                Scaffold { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -30,18 +29,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    RecipeComposeAppTheme {
-        Greeting("Android")
+fun RecipeComposeAppPreview() {
+    MaterialTheme {
+        Scaffold { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
