9a2fec715416059a1fa6c402649b069b610e9f86
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-08-23 12:06:10 +0500

    RCA-10: WIP

---
+++ app/src/main/java/com/renegatemaster/recipecomposeapp/ui/core/ScreenHeader.kt
@@ -0,0 +1,68 @@
+package com.renegatemaster.recipecomposeapp.ui.core
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.renegatemaster.recipecomposeapp.R
+
+@Composable
+fun ScreenHeader(header: String, imageRes: Int) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(224.dp),
+        contentAlignment = Alignment.BottomStart,
+    ) {
+        Image(
+            painter = painterResource(id = imageRes),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+        )
+        Surface(
+            modifier = Modifier
+                .wrapContentWidth()
+                .wrapContentHeight()
+                .padding(dimensionResource(R.dimen.basicIndentation)),
+            shape = RoundedCornerShape(
+                dimensionResource(R.dimen.basicIndentation)
+            ),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            Text(
+                text = header.uppercase(),
+                modifier = Modifier.padding(dimensionResource(R.dimen.headerPadding)),
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.displayLarge,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ScreenHeaderPreview() {
+    ScreenHeader(
+        header = "categories",
+        imageRes = R.drawable.bcg_categories
+    )
+}
--- app/src/main/java/com/renegatemaster/recipecomposeapp/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/renegatemaster/recipecomposeapp/ui/favorites/FavoritesScreen.kt
@@ -1,8 +1,7 @@
-package com.renegatemaster.recipecomposeapp.ui.categories
+package com.renegatemaster.recipecomposeapp.ui.favorites
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.renegatemaster.recipecomposeapp.R
@@ -10,14 +9,14 @@ import com.renegatemaster.recipecomposeapp.ui.core.ScreenBody
 import com.renegatemaster.recipecomposeapp.ui.core.ScreenHeader
 
 @Composable
-fun CategoriesScreen(modifier: Modifier) {
+fun FavoritesScreen(modifier: Modifier) {
     Column(
         modifier = modifier.fillMaxSize()
     ) {
         ScreenHeader(
-            header = "categories",
-            imageRes = R.drawable.bcg_categories
+            header = "favorites",
+            imageRes = R.drawable.bcg_favorites
         )
-        ScreenBody("There is no categories yet")
+        ScreenBody("There is no favorites yet")
     }
 }
