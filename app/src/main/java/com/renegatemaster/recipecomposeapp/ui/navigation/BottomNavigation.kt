62d128b9a490b4f9fcae2914c953b708bce09659
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-09-07 20:03:31 +0500

    RCA-14: Create Composable components and CategoriesScreen

--- app/src/main/java/com/renegatemaster/recipecomposeapp/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/renegatemaster/recipecomposeapp/ui/navigation/BottomNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -14,7 +15,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.renegatemaster.recipecomposeapp.R
+import com.renegatemaster.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 @Composable
 fun BottomNavigation(
@@ -47,6 +52,7 @@ fun BottomNavigation(
             Text(
                 text = "categories".uppercase(),
                 style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.surface,
             )
         }
         Button(
@@ -66,7 +72,25 @@ fun BottomNavigation(
             Text(
                 text = "favorites".uppercase(),
                 style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.surface,
+            )
+            AsyncImage(
+                model = R.drawable.ic_heart_empty,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(start = 10.dp)
+                    .size(24.dp),
             )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationPreview() {
+    RecipeComposeAppTheme {
+        BottomNavigation(
+            {}, {}
+        )
     }
 }
